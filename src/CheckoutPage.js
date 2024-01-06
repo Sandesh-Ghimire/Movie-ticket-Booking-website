690a7a63d9cf81145e5084d50d7aa821ab568417
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-01-06 15:04:31 +0545

    fix: data not showing

--- src/CheckoutPage.js
+++ src/CheckoutPage.js
@@ -1,26 +1,41 @@
 // CheckoutPage.js
-import React from 'react';
-import CheckoutForm from './CheckoutForm';
-import CheckoutSummary from './CheckoutSummary';
-import { useLocation } from 'react-router-dom';
-import './checkoutPage.css'; // Import the CSS file
+import React, { useState } from "react";
+import CheckoutForm from "./CheckoutForm";
+import CheckoutSummary from "./CheckoutSummary";
+import { useLocation } from "react-router-dom";
+import "./checkoutPage.css"; // Import the CSS file
 
 const CheckoutPage = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const title = queryParams.get('title');
-  const totalAmount = queryParams.get('totalAmount');
+  const title = queryParams.get("title");
+  const totalAmount = queryParams.get("totalAmount");
+
+  const [name, setName] = useState("*");
+  const [email, setEmail] = useState("*");
+  const [address, setAddress] = useState("*");
+  const [country, setCountry] = useState("*");
 
   return (
     <div className="checkout-page">
       <div className="left-side">
-   
-        <CheckoutForm />
+        <CheckoutForm
+          setName={setName}
+          setEmail={setEmail}
+          setAddress={setAddress}
+          setCountry={setCountry}
+        />
       </div>
-    
-      <div className="">
-     
-        <CheckoutSummary title={title} totalAmount={totalAmount} />
+
+      <div>
+        <CheckoutSummary
+          title={title}
+          totalAmount={totalAmount}
+          name={name === "" ? "*" : name}
+          email={email === "" ? "*" : email}
+          address={address === "" ? "*" : address}
+          country={country === "" ? "*" : country}
+        />
       </div>
     </div>
   );
